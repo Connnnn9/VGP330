21a52f021210f50898f56826635bc5a756616541
Connnnn9 <connnnn196470@gmail.com>
2024-02-07 15:50:36 -0800

    Initial commit

---
+++ 08_HelloLight/GameState.cpp
@@ -0,0 +1,96 @@
+#include"GameState.h"
+
+using namespace SpringEngine;
+using namespace SpringEngine::Graphics;
+using namespace SpringEngine::Math;
+using namespace SpringEngine::Input;
+
+void GameState::Initialize()
+{
+	mCamera.SetPosition({ 0.0f,1.0f,-4.0f });
+	mCamera.SetLookAt({ 0.0f,0.0f,0.0f });
+
+	mDirectionalLight.direction = Math::Normalize({ 1.0f,-1.0f,1.0f });
+	mDirectionalLight.ambient = { 0.5f,0.5f,0.5f,1.0f };
+	mDirectionalLight.diffuse = { 0.8f,0.8f,0.8f,1.0f };
+	mDirectionalLight.specular = { 1.0f,1.0f,1.0f,1.0f };
+
+	mSphere = MeshBuilder::CreateSphere(60, 60, 1.0f);
+
+	mEarth.meshBuffer.Initialize(mSphere);
+	mEarth.diffuseMapId = TextureManager::Get()->LoadTexture("earth.jpg");
+	mEarth.normalMapId = TextureManager::Get()->LoadTexture("earth_normal.jpg");
+
+	mStandardEffect.Initialize(L"../../Assets/Shaders/Standard.fx");
+	mStandardEffect.SetCamera(mCamera);
+	mStandardEffect.SetDirectionalLight(mDirectionalLight);
+}
+void GameState::Terminate()
+{
+	mStandardEffect.Terminate();
+	mEarth.Terminate();
+}
+void GameState::Update(const float deltaTime)
+{
+	UpdateCameraControl(deltaTime);
+}
+void GameState::Render()
+{
+	mStandardEffect.Begin();
+	mStandardEffect.Render(mEarth);
+	mStandardEffect.End();
+}
+void GameState::DebugUI()
+{
+	ImGui::Begin("Debug control", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+	if (ImGui::CollapsingHeader("Light", ImGuiTreeNodeFlags_DefaultOpen))
+	{
+		if (ImGui::DragFloat3("Direction",&mDirectionalLight.direction.x,0.1f))
+		{
+			mDirectionalLight.direction = Math::Normalize(mDirectionalLight.direction);
+		}
+
+		ImGui::ColorEdit4("Ambient", &mDirectionalLight.ambient.r);
+		ImGui::ColorEdit4("Diffuse", &mDirectionalLight.diffuse.r);
+		ImGui::ColorEdit4("Specular", &mDirectionalLight.specular.r);
+	}
+	ImGui::Separator();
+	mStandardEffect.DebugUI();
+	ImGui::End();
+}
+void GameState::UpdateCameraControl(float deltaTime)
+{
+	auto input = Input::InputSystem::Get();
+	const float moveSpeed = input->IsKeyDown(KeyCode::LSHIFT) ? 10.0f : 1.0f;
+	const float turnSpeed = 0.1f;
+
+	if (input->IsKeyDown(KeyCode::W))
+	{
+		mCamera.Walk(moveSpeed * deltaTime);
+	}
+	else if (input->IsKeyDown(KeyCode::S))
+	{
+		mCamera.Walk(-moveSpeed * deltaTime);
+	}
+	if (input->IsKeyDown(KeyCode::D))
+	{
+		mCamera.Strafe(moveSpeed * deltaTime);
+	}
+	else if (input->IsKeyDown(KeyCode::A))
+	{
+		mCamera.Strafe(-moveSpeed * deltaTime);
+	}
+	if (input->IsKeyDown(KeyCode::E))
+	{
+		mCamera.Rise(moveSpeed * deltaTime);
+	}
+	else if (input->IsKeyDown(KeyCode::Q))
+	{
+		mCamera.Rise(-moveSpeed * deltaTime);
+	}
+	if (input->IsMouseDown(MouseButton::RBUTTON))
+	{
+		mCamera.Yaw(input->GetMouseMoveX() * turnSpeed * deltaTime);
+		mCamera.Pitch(input->GetMouseMoveY() * turnSpeed * deltaTime);
+	}
+}
